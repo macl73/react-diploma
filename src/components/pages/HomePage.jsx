4d07c7dc0fa8c32bba15197de7942c35ef7ce657
macl73 <la-paev@yandex.ru>
2023-05-31 20:20:36 +0400

    final milestone

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import SalesHits from '../SalesHits.jsx';
 import Catalog from './Catalog.jsx';
 
+import { useDispatch } from 'react-redux';
+import { search } from '../../slices/search.js';
+import { searchInput } from '../../slices/searchInput.js';
+
+
 export default function HomePage() {
+
+    const dispatch = useDispatch();
+    dispatch(searchInput(""));
+    dispatch(search(undefined));
+
     return (
         <>
             <SalesHits />
